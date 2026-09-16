268bee7ca4cef0c0bd8b23eafbe1020df0deff74
ayush <124794246+ayushzjjjjzk@users.noreply.github.com>
2026-09-17 01:06:35 +0530

    first commit of the project adding all the prizma and other thing

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage()  {
+    return (
+        <main>
+            <h1>CashTrail</h1>
+            <p> Your expenses, clearly explained </p>
+            <h1>this is ayush singh ramola working on this project </h1>
+        </main>
+    )
+}
